906e1570c4b8c2b6fe731c3cf511c3bac0713dad
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2025-08-16 18:44:55 +0900

    feat: 이력서 업데이트

--- webpack.common.js
+++ webpack.common.js
@@ -7,6 +7,31 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    filename: './js/app.js',
-  }, 
+    filename: './js/[name].js',
+    chunkFilename: './js/[name].chunk.js',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+        mermaid: {
+          test: /[\\/]node_modules[\\/]mermaid[\\/]/,
+          name: 'mermaid',
+          chunks: 'all',
+          priority: 10,
+        },
+      },
+    },
+  },
+  performance: {
+    // 큰 asset들에 대한 경고 임계값 조정
+    maxAssetSize: 1000000, // 1MB
+    maxEntrypointSize: 1000000, // 1MB
+    hints: 'warning',
+  },
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,6 +4,12 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  optimization: {
+    ...common.optimization,
+    minimize: true,
+    usedExports: true,
+    sideEffects: false,
+  },
   plugins: [
     new CopyPlugin({
       patterns: [
@@ -13,8 +19,14 @@ module.exports = merge(common, {
         { from: 'index.html', to: 'index.html' },
         { from: 'img', to: 'img' },
         { from: 'css', to: 'css' },
-        { from: 'js/vendor', to: 'js/vendor' },
-        { from: 'js/vendor/mermaid.min.js', to: 'js/mermaid.js' },
+        // Copy vendor files except mermaid (will be bundled separately)
+        { 
+          from: 'js/vendor', 
+          to: 'js/vendor',
+          globOptions: {
+            ignore: ['**/mermaid.min.js']
+          }
+        },
         { from: 'icon.svg', to: 'icon.svg' },
         { from: 'favicon.ico', to: 'favicon.ico' },
         { from: 'favicon.svg', to: 'favicon.svg' },
@@ -22,8 +34,22 @@ module.exports = merge(common, {
         { from: 'icon.png', to: 'icon.png' },
         { from: '404.html', to: '404.html' },
         { from: 'site.webmanifest', to: 'site.webmanifest' },
-        // copy local font files for offline use
-        { from: 'node_modules/@fontsource/noto-sans-kr/files', to: 'fonts/noto-sans-kr' },
+        // copy only necessary font weights for offline use (400, 700)
+        { 
+          from: 'node_modules/@fontsource/noto-sans-kr/files', 
+          to: 'fonts/noto-sans-kr',
+          globOptions: {
+            ignore: [
+              '**/noto-sans-kr-korean-100-normal.*',
+              '**/noto-sans-kr-korean-200-normal.*', 
+              '**/noto-sans-kr-korean-300-normal.*',
+              '**/noto-sans-kr-korean-500-normal.*',
+              '**/noto-sans-kr-korean-600-normal.*',
+              '**/noto-sans-kr-korean-800-normal.*',
+              '**/noto-sans-kr-korean-900-normal.*'
+            ]
+          }
+        },
       ],
     }),
   ],
